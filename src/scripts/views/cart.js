c4e9c39f315325b86a013e54b98b728ee904a7ea
hotwkf <349210796@qq.com>
2016-08-21 12:58:20 +0800

    8.21-wkf-1

--- src/scripts/views/cart.js
+++ src/scripts/views/cart.js
@@ -4,25 +4,66 @@ var cartTpl = require('../tpls/cart.string');
 SPA.defineView('cart', {
   	// 装载模板
   	html: cartTpl,
-	bindEvents: {
+bindEvents: {
 	'show': function () {
-		
-		/*touch.on('#target', 'touchstart', function(ev){
-			ev.preventDefault();
-		});
-		
-		var target = document.getElementById("target");
-		target.style.webkitTransition = 'all ease 0.2s';
-		
-		touch.on(target, 'swiperight', function(ev){
-			this.style.webkitTransform = "translate3d(" + rt + "px,0,0)";
-			log("向右滑动.");
-		});
-		
-		touch.on(target, 'swipeleft', function(ev){
-			log("向左滑动.");
-			this.style.webkitTransform = "translate3d(-" + this.offsetLeft + "px,0,0)";
-		});*/
+		var n=0;
+		var num=$("#wkf_add span:nth-child(2)").html();
+		var prie=$("#wkf_right p:last-child i:last-child").html();
+		var tol=$("#zj").html();
+		var tol2=$("#zj2").html();
+		var cj=$("#cj").html();
+		var chajia=parseFloat(cj-tol);
+//		console.log(chajia);
+		console.log(cj)
+		console.log(tol)
+		$("#wkf_add span:first-child").on("tap",function(){
+//			console.log(prie)
+//			var chajia=parseFloat(cj-tol);
+			if(chajia<=0){
+				$("#myf").show();
+				$("#yf").hide();
+			}
+			console.log(chajia);
+			if(n>=1){
+				
+				n--;
+				prie=parseFloat(prie)
+				num=parseFloat(num)-1;
+				tol=prie*num;
+				tol2=tol;
+				if(tol2>=299){
+					tol2=tol2-10
+				}
+				$("#wkf_add span:nth-child(2)").html(num);
+				$("#zj").html(tol);
+				$("#zj2").html(tol2);
+				
+			}
+			
+		})
+		$("#wkf_add span:last-child").on("tap",function(){
+			if(chajia<=0){
+				$("#myf").show();
+				$("#yf").hide();
+			}
+			n++;
+			console.log(chajia);
+				prie=parseFloat(prie)
+				num=parseFloat(num)+1;
+				tol=prie*num;
+				tol2=tol;
+				if(tol2>=299){
+					tol2=tol2-10
+				}
+				
+				$("#wkf_add span:nth-child(2)").html(num);
+				$("#zj").html(tol);
+				$("#zj2").html(tol2);
+				
+		})
+				
 	}
 }
+ 
+ 
 });
