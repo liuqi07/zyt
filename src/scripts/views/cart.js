ef096b356e7ee20f01e64c3b98a88d91d748b26a
hotwkf <349210796@qq.com>
2016-08-21 14:53:52 +0800

    8.21-wkf-2

--- src/scripts/views/cart.js
+++ src/scripts/views/cart.js
@@ -7,59 +7,68 @@ SPA.defineView('cart', {
 bindEvents: {
 	'show': function () {
 		var n=0;
+		var by = 240;
 		var num=$("#wkf_add span:nth-child(2)").html();
 		var prie=$("#wkf_right p:last-child i:last-child").html();
 		var tol=$("#zj").html();
 		var tol2=$("#zj2").html();
 		var cj=$("#cj").html();
 		var chajia=parseFloat(cj-tol);
-//		console.log(chajia);
+		$("#cj").html(chajia)
 		console.log(cj)
 		console.log(tol)
-		$("#wkf_add span:first-child").on("tap",function(){
-//			console.log(prie)
-//			var chajia=parseFloat(cj-tol);
-			if(chajia<=0){
-				$("#myf").show();
+			if(cj<=tol){
+				
 				$("#yf").hide();
+				$("#myf").show();
+			}else{
+				$("#yf").show();
+				$("#myf").hide();
 			}
-			console.log(chajia);
-			if(n>=1){
-				
-				n--;
-				prie=parseFloat(prie)
-				num=parseFloat(num)-1;
-				tol=prie*num;
-				tol2=tol;
-				if(tol2>=299){
-					tol2=tol2-10
-				}
-				$("#wkf_add span:nth-child(2)").html(num);
-				$("#zj").html(tol);
-				$("#zj2").html(tol2);
-				
+		$("#wkf_add span:first-child").on("tap",function(){
+			num--;
+			if(num<1){
+				num=1;
+				return;
 			}
-			
+			$("#wkf_add span:nth-child(2)").html(num);
+			tol = num * prie;
+			$("#zj").html(tol);
+			chajia = tol - by;
+			if(chajia<=0){
+				$("#cj").html(Math.abs(chajia));
+				$("#myfl").html("10.0");
+				$("#yf").show();
+				$("#myf").hide();
+			}else{
+				$("#myfl").html(0);
+				$("#yf").hide();
+				$("#myf").show();
+			}
+			tol2=tol;
+			if(tol2>=299){
+				tol2=tol2-10
+			}	
+			$("#zj2").html(tol2);
 		})
 		$("#wkf_add span:last-child").on("tap",function(){
+			
+			num++;
+			$("#wkf_add span:nth-child(2)").html(num);
+			tol = num*prie;
+			$("#zj").html(tol);
+			chajia = cj - tol;
+			$("#cj").html(chajia);
 			if(chajia<=0){
-				$("#myf").show();
 				$("#yf").hide();
+				$("#myf").show();
+				$("#myfl").html(0);
 			}
-			n++;
-			console.log(chajia);
-				prie=parseFloat(prie)
-				num=parseFloat(num)+1;
-				tol=prie*num;
-				tol2=tol;
-				if(tol2>=299){
-					tol2=tol2-10
-				}
-				
-				$("#wkf_add span:nth-child(2)").html(num);
-				$("#zj").html(tol);
-				$("#zj2").html(tol2);
-				
+			tol2=tol;
+			if(tol2>=299){
+				tol2=tol2-10
+			}	
+			$("#zj2").html(tol2);
 		})
 				
 	}
