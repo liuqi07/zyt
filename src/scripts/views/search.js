50d83c7b2e0fd5fcadc6227817ef323f50ad8881
liuqi07 <18911690880@163.com>
2016-08-25 02:10:50 +0800

    lq

--- src/scripts/views/search.js
+++ src/scripts/views/search.js
@@ -4,34 +4,42 @@ var searchTpl = require('../tpls/search.string');
 SPA.defineView('search', {
   	// 装载模板
   	html: searchTpl,
-	
+
     plugins: [{
 	    name: 'avalon',
 	    options: function (vm) {
 	    	vm.search = [];
 	    }
 	},'delegated'],
-		
+
     bindActions: {
         'goto.cart': function () {
             SPA.open("cart");
         },
         'back': function () {
             this.hide();
+        },
+        'goto.good': function(el, data){
+            console.log(el+' '+data);
+            SPA.open('good', {
+                param: {
+                    id: data.id
+                }
+            })
         }
     },
     bindEvents: {
     	'show': function(){
     		var vm = this.getVM();
     		var $search = $("#search-container>header>.search>input");
-    		
+
     		$search.on('blur',function(){
     			var txt = $search.val();
     			$.ajax({
     				url: '/api/allGoods.php',
     				type: 'get',
     				success: function(res){
-    					console.log(res.data.items);
+    					//console.log(res.data.items);
     					var json = res.data.items;
     					var arr = [];
     					$(json).each(function(i){
@@ -40,7 +48,7 @@ SPA.defineView('search', {
     							arr.push($(json).eq(i)[0]);
     						}
     					});
-    					console.log(arr);
+    					//console.log(arr);
     					if(arr.length==0){
     						alert("没有搜索到任何商品！");
     					}else{
