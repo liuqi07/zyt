3d3fc9a1199b1a5206c83e9d7153468ce26a83c0
wushaof <1551081714@qq.com>
2016-08-25 10:49:37 +0800

    wsf

--- src/scripts/views/login.js
+++ src/scripts/views/login.js
@@ -12,6 +12,29 @@ SPA.defineView('login', {
         },
         'back': function () {
             this.hide();
+        },
+        "login_":function(){
+          var user1=$("#tel").val();
+          var pass1=$("#pass").val();
+          $.ajax({
+              url:"http://datainfo.duapp.com/shopdata/userinfo.php",
+              data:{
+                  status:"login",
+                  userID:user1,
+                  password:pass1
+
+              },
+              success:function(data){
+                  if(data==0){
+                      $(".bm1").html("用户名不存在！")
+                  }else if(data==2){
+                      $(".bm1").html("密码输入有误！")
+                  }else{
+                    $(".bm1").html("登录成功！");
+                    SPA.open('home');
+                  }
+              }
+          })
         }
     }
 
